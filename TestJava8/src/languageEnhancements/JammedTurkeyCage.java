5d15d5d2a4126b7ad5eb84291c8939b061bad01d
rafael <rafael.machado@camara.gov.br>
2017-01-13 17:43:38 -0200

    Correção

--- TestJava8/src/languageEnhancements/JammedTurkeyCage.java
+++ TestJava8/src/languageEnhancements/JammedTurkeyCage.java
@@ -17,13 +17,13 @@ public class JammedTurkeyCage implements AutoCloseable {
 //		}
 
 		System.out.println("###2");
-//		try (JammedTurkeyCage t1 = new JammedTurkeyCage(); JammedTurkeyCage t2 = new JammedTurkeyCage()) {
-//			System.out.println("turkeys entered cages");
-//		} catch (IllegalStateException e) {
-//			System.out.println("caught: " + e.getMessage());
-//			for (Throwable t : e.getSuppressed())
-//				System.out.println(t.getMessage());
-//		}
+		try (JammedTurkeyCage t1 = new JammedTurkeyCage(); JammedTurkeyCage t2 = new JammedTurkeyCage()) {
+			throw new IllegalStateException("turkeys entered cages");
+		} catch (IllegalStateException e) {
+			System.out.println("caught: " + e.getMessage());
+			for (Throwable t : e.getSuppressed())
+				System.out.println(t.getMessage());
+		}
 
 		
 
